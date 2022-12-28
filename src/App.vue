d8158ac985ff55ea39b3a519ed0df6c2b94d3e5f
Bouzid Krita <bouzid.krita@habiteo.com>
2022-12-28 17:02:38 +0100

    update app

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,12 @@ export default {
 
 <style>
 #app {
-  /* prendre toute la taille de la fenetre*/
-  
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #f2f2f2;
   background: #2c3e50;
-  height: 100vh;
-  width: 100vw;
 }
 </style>
